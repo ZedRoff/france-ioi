e41c561a70087e335e005fb4053be2b8ba588c15
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-04 15:28:49 +0200

    Create exercice_3.py

---
+++ Level_1/part_1/exercice_3.py
@@ -0,0 +1,7 @@
+# We can do a for loop here, to write better code, but I will do this the way they want me to do it, because it can cause errors in the website, and I'm not sure about if they accept advanced tricks. So let's just print each of them.
+
+print("Tout droit tu grimperas,")
+print("La clé tu trouveras,")
+print("Habile tu seras,")
+print("Quand tu les porteras,")
+print("Et avec le chef tu reviendras !")
