446c9e649c967a84d9e35875f7e4482c1cefff9f
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-04 17:42:31 +0200

    Create exercice_5.py

---
+++ Level_1/part_3/exercice_5.py
@@ -0,0 +1,10 @@
+batton_1 = 17
+batton_2 = 7
+batton_3 = 5
+batton_4 = 2
+cote_cour = (5 * batton_1) + (2 * batton_2) + (1 * batton_3) + (2 * batton_4)
+surface = cote_cour * cote_cour
+perimetre = cote_cour * 4
+
+print(surface)
+print(perimetre)
