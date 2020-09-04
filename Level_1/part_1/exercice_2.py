61e35d5b58e46c65e8dc723b15bdd3e84aad3a24
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-04 15:24:26 +0200

    Update exercice_2.py

--- Level_1/part_1/exercice_2.py
+++ Level_1/part_1/exercice_2.py
@@ -1,3 +1,6 @@
+# It's unorderder, we want to reorder it, so we just need to change line's places.
+
+
 print("Coucou !")
 print("Je m'appelle Camthalion")
 print("Ma devise est 'Parler peu mais parler bien'.")
