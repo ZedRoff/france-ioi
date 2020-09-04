b9d9ac37f819d303624a8fc1cb57a3478cdd2379
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-04 17:14:13 +0200

    Create exercice_8.py

---
+++ Level_1/part_2/exercice_8.py
@@ -0,0 +1,10 @@
+from robot import *
+for x in range(0, 108, 1):
+  for i in range(0, 13, 1):
+    haut()
+  for i in range(0, 13, 1):
+    droite()
+  for i in range(0, 13, 1):
+    bas()
+  for i in range(0, 13, 1):
+    gauche()
