068993f85ed0f50b5edd84c8c1413c4c7f309764
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-04 16:19:20 +0200

    Create exercice_6.py

---
+++ Level_1/part_1/exercice_6.py
@@ -0,0 +1,9 @@
+from robot import *
+
+remplir(5) # 5 = 5 & 3 = 0
+transferer(5, 3) # 5 = 2 & 3 = 3
+vider(3) # 5 = 2 & 3 = 0
+transferer(5, 3) # 5 = 0 & 3 = 2
+remplir(5) # 5 = 5 & 3 = 2
+transferer(5, 3) # 5 = 4 & 3 = 3
+
