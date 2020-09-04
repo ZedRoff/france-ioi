71997adfbb2884bce97bb129111097f98b3b0b13
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-04 15:32:43 +0200

    Create exercice_4.py

---
+++ Level_1/part_1/exercice_4.py
@@ -0,0 +1,11 @@
+# Here the error is that the robot goes left at a moment, so we need to continue the code at this point.
+
+from robot import *
+haut()
+haut()
+haut()
+droite()
+droite()
+bas()
+bas()
+droite()
