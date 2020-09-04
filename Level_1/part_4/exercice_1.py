a0f46e647cb45f1a9b710c261cc16a83961e8e22
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-04 19:31:44 +0200

    Create exercice_1.py

---
+++ Level_1/part_4/exercice_1.py
@@ -0,0 +1,11 @@
+positionDepart = int(input())
+largeurEmplacement = int(input())
+nbVendeurs = int(input())
+i = 0
+while i-1 < nbVendeurs:
+  
+  
+  print(positionDepart)
+  positionDepart+=largeurEmplacement
+  i+=1
+  
