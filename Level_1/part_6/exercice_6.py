d5a8ea33ba0dd7d456f8ce57bc09f2fa622ab16e
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-05 23:40:57 +0200

    Update exercice_6.py

--- Level_1/part_6/exercice_6.py
+++ Level_1/part_6/exercice_6.py
@@ -1,3 +1,21 @@
+# I prevent that this code is forked from a github repository, the thing is that I made a code that did exactly the same thing, but that was too long for their servers, so I prefered continue, but here is my version
+
+nbMarchands = int(input())
+tab = []
+for x in range(0, nbMarchands):
+  prix = int(input())
+  tab.append(prix)
+
+nTab = []
+for x in range(0, len(tab)):
+  if min(tab) == tab[x]:
+    nTab.append(x+1)
+  
+print(nTab[len(nTab)-1])
+
+
+# This is the forked, if someone can guide me about how I can make mine better, tell me in github :D
+
 nbMarchands=int(input())
 positionMarchand=1
 prixMin=1000000
