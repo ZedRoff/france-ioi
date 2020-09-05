54338eee2e68b4c9c534cb32412f51b9bcfefdc5
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-05 23:05:16 +0200

    Create exercice_5.py

---
+++ Level_1/part_6/exercice_5.py
@@ -0,0 +1,14 @@
+e1 = int(input())
+e2 = int(input())
+
+
+if e1 <=5 and e2 >= 8:
+  print("Tinuviel")
+elif e1 >=10 and e2 >= 10:
+  print("Calaelen")
+elif e1 <=8 and e2 <=5:
+  print("Falarion")
+elif e1 >= 12 and e2 >= 7:
+  print("Dorthonion")
+else:
+  print("Dorthonion")
