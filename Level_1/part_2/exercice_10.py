381837ea91fc01cd383167c3f4bba919c4b4e79d
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2020-09-11 18:35:09 +0200

    Create exercice_10.py

---
+++ Level_1/part_2/exercice_10.py
@@ -0,0 +1,45 @@
+from robot import *
+
+for x in range(0, 9, 1):
+  haut()
+
+droite()
+for x in range(0, 8, 1):
+  bas()
+droite()
+
+for x in range(0, 8, 1):
+  haut()
+
+droite()
+  
+
+for x in range(0, 8, 1):
+  bas()
+  
+droite()
+for x in range(0, 8, 1):
+  haut()
+
+droite()
+
+for x in range(0, 8, 1):
+  bas()
+droite()
+
+for x in range(0, 8, 1):
+  haut()
+droite()
+
+for x in range(0, 8, 1):
+  bas()
+droite()
+
+for x in range(0, 8, 1):
+  haut()
+droite()
+
+for x in range(0, 9, 1):
+  bas()
+for x in range(0, 9, 1):
+  gauche()
